5207ca07457a17ff7b2decf04c054e16da7ae4ba
jvckthedog <jmccollum26@gmail.com>
2025-04-03 15:35:44 -0700

    inital commit

---
+++ intro-to-ruby.rb
@@ -0,0 +1,66 @@
+# Writing variables
+age = 4 
+
+age /= age + 18.0
+puts age 
+
+
+where_do_i_live = "vancouver"
+maddie_lives = where_do_i_live
+puts maddie_lives
+
+howFastAmIGoing = 60 
+letsGoFaster = howFastAmIGoing + 70
+puts letsGoFaster
+
+letsGoSlower = howFastAmIGoing / 2
+puts letsGoSlower 
+
+fiveSomewhere = 4
+whatTimeIsIt = 1 
+time = fiveSomewhere + whatTimeIsIt
+puts time
+
+name = "Jordan" 
+puts "hello " + name 
+
+#print & Puts 
+#print
+dogsName = "Jack"
+print dogsName
+
+dogsAge = 3
+print dogsAge
+
+#puts 
+catsName = "Z"
+puts catsName
+
+catsAge = 100
+puts catsAge
+
+#Conditional Logic
+
+
+puts "Hot diggity damn, 1 is less than 2" if 1 < 2
+
+#case Statments
+grade = "f"
+did_i_pass = case grade
+  when 'a' then puts "hell yeah"
+  when 'd' then puts "not good"
+  else puts "you shall not pass"
+end
+
+puts did_i_pass
+
+#unless Statements
+age = 19
+unless age < 18
+  puts "Get a job."
+end
+
+
+
+
+
