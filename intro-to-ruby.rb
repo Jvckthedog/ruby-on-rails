62ef2d36a01969487de11753663ad94d42f3db13
jvckthedog <jmccollum26@gmail.com>
2025-04-07 08:38:58 -0700

    update intro to ruby - 4.7.25

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -60,6 +60,39 @@ unless age < 18
   puts "Get a job."
 end
 
+#loops
+i = 0 
+loop do 
+  puts "i is #{i}"
+  i +=1 
+  break if i == 10
+end
+
+#arrays
+#basic arrays
+num_array = {1,2}
+str_array = {"this", "is", "a", "small", "array"}
+
+#creating arrays (Captial "A" is important, it work without.)
+Array.new #=> []
+Array.new(3) #=> [nil,nil,nil]
+Array.new(3, "hello world") #=> [hello world, hello world, hello world]
+
+#accessing arrays
+str_array[0] #=> "this"
+str_array[-2] #=> "small"
+
+#array - .first, .last
+str_array.first[3] #=> "this", "is", "a"
+str_array.last[2] #=> "small", "array"
+
+#adding and removing arrays
+num_array.push(3, 4) #=> [1, 2, 3, 4]
+num_array << 5 #=> [1, 2, 3, 4, 5]
+
+
+
+
 
 
 
